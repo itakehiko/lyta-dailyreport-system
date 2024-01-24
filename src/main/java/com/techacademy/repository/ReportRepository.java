3075a44c89a591f73f02d144db305e8095af6e0d
itakehiko <takehiko.iwasaki.24@gmail.com>
2024-01-24 22:06:25 +0900

    Repository修正

--- src/main/java/com/techacademy/repository/ReportRepository.java
+++ src/main/java/com/techacademy/repository/ReportRepository.java
@@ -2,6 +2,8 @@ package com.techacademy.repository;
 
 import com.techacademy.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import  org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReportRepository extends JpaRepository<Report, Integer> {
 }
